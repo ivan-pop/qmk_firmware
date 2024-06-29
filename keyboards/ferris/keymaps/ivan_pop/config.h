a845f76c95f10000074c84ca1a73ce4678f6cf3b
Ivan Popovych <ivan@ipvych.com>
2024-06-29 16:57:28 +0300

    try to reduce latency by forcing nkro & changing debounce type
    Not sure which of these options have most effect but none of them seem
    to have negative effects so just enable all of them

--- keyboards/ferris/keymaps/ivan_pop/config.h
+++ keyboards/ferris/keymaps/ivan_pop/config.h
@@ -39,3 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
+
+#define FORCE_NKRO
+#define USB_POLLING_INTERVAL_MS 1
